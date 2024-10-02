65ca7096b35889f87c185e5e0b58d97fd7860a37
Igor Kirpinev <kirpinevigor@yandex.ru>
2024-10-02 15:51:55 +0300

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -31,10 +31,6 @@ export const App = () => {
     window.gtag("event", "sub_hidden_3339_4_click");
   };
 
-  const clickApprove = () => {
-    window.gtag("event", "alfa_smart_3339_4_approve");
-  };
-
   const submit = () => {
     setLoading(true);
     sendDataToGA({
@@ -49,7 +45,6 @@ export const App = () => {
 
   const handleSelection = () => {
     if (selectedOption === Product.Smart) {
-      clickApprove()
       setShowSmart(true);
     }
 
